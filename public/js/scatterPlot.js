a31b7b9bf1a4ac465d7b553bd1c1308d770711b2
David Cian <cian_david@outlook.com>
2021-06-04 05:16:30 +0300

    Implement radar chart

--- public/js/scatterPlot.js
+++ public/js/scatterPlot.js
@@ -171,6 +171,25 @@ function update(features) {
     values: plot_values
   }
 
+  radar_values = []
+
+  playlist_counter = 0
+
+  user_data.forEach(function (playlist_data) {
+    for (feature of features) {
+      console.log("In loop, feature " + feature)
+      playlist_feature = {'key': feature, 'category': playlist_counter}
+      feature_value = 0
+      for (i = 0; i < playlist_data[features[0]].length; i++) {
+        feature_value += playlist_data[feature][i]
+      }
+      playlist_feature['value'] = Math.round(feature_value)
+      
+      radar_values.push(playlist_feature)
+    }
+    playlist_counter += 1
+  })
+
   view_specs = []
 
   for (x_feature of features) {
@@ -185,7 +204,172 @@ function update(features) {
 
   // TODO fix scrolling which zooms selection window and plot itself at the same time
 
-  vlSpec = {
+  radarSpec = {
+    $schema: "https://vega.github.io/schema/vega/v5.json",
+    width: 400,
+    height: 400,
+    padding: 40,
+    autosize: {type: 'none', contains: 'padding'},
+    signals: [
+      {name: 'radius', update: 'width / 2'}
+    ],
+    data: [
+      {
+        name: 'table',
+        values: radar_values
+      },
+      {
+        name: 'keys',
+        source: 'table',
+        transform: [
+          {
+            type: 'aggregate',
+            groupby: ['key']
+          }
+        ]
+      }
+    ],
+    scales: [
+      {
+        name: 'angular',
+        type: 'point',
+        range: {signal: '[-PI, PI]'},
+        padding: 0.5,
+        domain: {data: 'table', field: 'key'}
+      },
+      {
+        name: 'radial',
+        type: 'linear',
+        range: {signal: '[0, radius]'},
+        domain: {data: 'table', field: 'value'},
+        domainMin: 0
+      },
+      {
+        name: 'color',
+        type: 'ordinal',
+        domain: {data: 'table', field: 'category'},
+        range: {scheme: 'category10'}
+      }
+    ],
+    encode: {
+      enter: {
+        x: {signal: 'radius'},
+        y: {signal: 'radius'}
+      }
+    },
+    marks: [
+      {
+        type: 'group',
+        name: 'categories',
+        zindex: 1,
+        from: {
+          facet: {data: 'table', name: 'facet', groupby: 'category'}
+        },
+        marks: [
+          {
+            type: 'line',
+            name: 'category-line',
+            from: {data: 'facet'},
+            encode: {
+              enter: {
+                interpolate: {value: 'linear-closed'},
+                x: {signal: "scale('radial', datum.value) * cos(scale('angular', datum.key))"},
+                y: {signal: "scale('radial', datum.value) * sin(scale('angular', datum.key))"},
+                stroke: {scale: 'color', field: 'category'},
+                strokeWidth: {value: 1},
+                fill: {scale: 'color', field: 'category'},
+                fillOpacity: {value: 0.1}
+              }
+            }
+          },
+          /*{
+            type: 'text',
+            name: 'value-text',
+            from: {data: 'category-line'},
+            encode: {
+              enter: {
+                x: {signal: "datum.x"},
+                y: {signal: "datum.y"},
+                text: {signal: "datum.datum.value"},
+                align: {value: 'center'},
+                baseline: {value: 'middle'},
+                fill: {value: 'white'}
+              }
+            }
+          }*/
+        ]
+      },
+      {
+        type: 'rule',
+        name: 'radial-grid',
+        from: {data: 'keys'},
+        zindex: 0,
+        encode: {
+          enter: {
+            x: {value: 0},
+            y: {value: 0},
+            x2: {signal: "radius * cos(scale('angular', datum.key))"},
+            y2: {signal: "radius * sin(scale('angular', datum.key))"},
+            stroke: {value: 'lightgrey'},
+            strokeWidth: {value: 1}
+          }
+        }
+      },
+      {
+        type: 'text',
+        name: 'key-label',
+        from: {data: 'keys'},
+        zindex: 1,
+        encode: {
+          enter: {
+            x: {signal: "(radius + 5) * cos(scale('angular', datum.key))"},
+            y: {signal: "(radius + 5) * sin(scale('angular', datum.key))"},
+            text: {field: 'key'},
+            align: [
+              {
+                test: "abs(scale('angular', datum.key)) > PI / 2",
+                value: 'right'
+              },
+              {
+                value: 'left'
+              }
+            ],
+            baseline: [
+              {
+                test: "scale('angular', datum.key) > 0",
+                value: 'top'
+              },
+              {
+                test: "scale('angular', datum.key) == 0",
+                value: 'middle'
+              },
+              {
+                value: 'bottom'
+              }
+            ],
+            fill: {value: 'white'},
+            fontWeight: {value: 'bold'}
+          }
+        }
+      },
+      {
+        type: 'line',
+        name: 'outer-line',
+        from: {data: 'radial-grid'},
+        encode: {
+          enter: {
+            interpolate: {value: 'linear-closed'},
+            x: {field: 'x2'},
+            y: {field: 'y2'},
+            stroke: {value: 'white'},
+            strokeWidth: {value: 1}
+          }
+        }
+      }
+    ]
+  }
+
+  scatterSpec = {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
     data: plot_data,
     background: null,
@@ -221,10 +405,12 @@ function update(features) {
     concat: view_specs
   }
 
-  vegaEmbed(vlSpec, {actions: false}).then(res => document.getElementById('cross-correlation-grid').append(res))
+  // Embed the general and the detailed visualizations in the web page
+  vegaEmbed(radarSpec, {actions: false}).then(res => document.getElementById('cross-correlation-grid').append(res))
+  vegaEmbed(scatterSpec, {actions: false}).then(res => document.getElementById('cross-correlation-grid').append(res))
 }
 
-update(['acousticness', 'danceability']);
+update(['acousticness', 'danceability', 'speechiness']);
 
 // When the button is changed, run the updateChart function
 d3.select("#feature-select-button").on("change", function () {
@@ -232,7 +418,6 @@ d3.select("#feature-select-button").on("change", function () {
     let features = d3.select("#feature-select-button").property("selectedOptions");
     features = Array.from(features).map(el => el.value)
     // run the updateChart function with this selected option
-    console.log(features)
     update(features);
 });
 
